b3979d8c2be00226b20a8ef72e9abec0370a3324
Federico Lois <federico.lois@gmail.com>
2024-07-05 12:24:30 -0300

    RavenDB-22261: Accesses to trees are only able to be done through static methods.

--- src/Voron/Impl/LowLevelTransaction.cs
+++ src/Voron/Impl/LowLevelTransaction.cs
@@ -330,7 +330,7 @@ namespace Voron.Impl
         {
             if (_envRecord.Root != null)
             {
-                _root = new Tree(this, null, Constants.RootTreeNameSlice, _envRecord.Root);
+                _root = Tree.GetRoot(this, Constants.RootTreeNameSlice, _envRecord.Root);
             }
         }
 
