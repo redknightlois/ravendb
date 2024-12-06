78256498cadeeace6026ff9c122f0b4fec1bf2a2
Ayende Rahien <ayende@ayende.com>
2024-12-06 15:17:14 +0200

    RavenDB-23085 - Started to implement shared jounrals - We can now submit a journal write from a branch env to a root env, which will record it in its own journal

--- src/Voron/StorageEnvironmentOptions.cs
+++ src/Voron/StorageEnvironmentOptions.cs
@@ -1370,6 +1370,12 @@ namespace Voron
         }
 
         internal TestingStuff ForTestingPurposes;
+        
+        /// <summary>
+        /// This is used when we have a branch environment, whose journal
+        /// is actually managed by a root environment
+        /// </summary>
+        public WriteAheadJournal RootJournal;
 
         internal TestingStuff ForTestingPurposesOnly()
         {
