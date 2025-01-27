0c382613357fb74444d05c8d28880151d644c869
Ayende Rahien <Ayende@ayende.com>
2025-01-27 10:32:14 +0200

    RavenDB-23085 - Updating binaries

--- src/Sparrow.Server/Platform/Pal.cs
+++ src/Sparrow.Server/Platform/Pal.cs
@@ -11,7 +11,7 @@ namespace Sparrow.Server.Platform
     public static unsafe class Pal
     {
         
-        public const int PAL_VER = 70128; // Should match auto generated rc from rvn_get_pal_ver() @ src/rvngetpalver.c
+        public const int PAL_VER = 70129; // Should match auto generated rc from rvn_get_pal_ver() @ src/rvngetpalver.c
 
         static Pal()
         {
